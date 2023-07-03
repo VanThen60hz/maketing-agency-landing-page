79165e6d1a7215fc1f85168d65d3f00df97d75f1
VanThen60hz <nguyenvthang2409@gmail.com>
2023-07-03 17:17:28 +0700

    landing page

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * navbar variables
+ */
+const menuToggleBtn = document.querySelector("[data-navbar-toggle-btn]");
+const navbar = document.querySelector("[data-navbar]");
+
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
+
+menuToggleBtn.addEventListener("click", function () {
+  elemToggleFunc(navbar);
+});
